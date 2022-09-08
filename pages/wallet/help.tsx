c44b61d44cff2afac313ecdd4e3de680cb159204
TheMetaverseEngineer <info@themetaverse.engineer>
2022-09-08 04:02:28 -0700

    adding walletHelp page

--- pages/wallet/help.tsx
+++ pages/wallet/help.tsx
@@ -1,6 +1,10 @@
+import { Divider, Typography } from "@mui/material";
+import Btn from "@shared/Btn/Btn";
+import CustomInput from "@shared/CustomInput/CustomInput";
 import WalletLayout from "@shared/Layouts/WalletLayout/WalletLayout";
 import Head from "next/head";
 import { NextPageWithLayout } from "../_app";
+import Questions from "components/FAQs/Questions/Questions";
 
 const Help: NextPageWithLayout = () => {
   return (
@@ -8,7 +12,47 @@ const Help: NextPageWithLayout = () => {
       <Head>
         <title>Albantsho || Help </title>
       </Head>
-      Help
+      <div className="bg-white rounded-md px-3 sm:px-6 py-6 md:px-10 md:py-9 lg:px-14 lg:py-14 flex-1 w-full">
+        <Typography
+          variant="h4"
+          className="text-primary-700 futura font-medium"
+        >
+          Withdraw
+        </Typography>
+        <Typography variant="body1" className="text-neutral-800 max-w-lg">
+          Your assets is safe with us and available to you whenever needed.
+          Please make sure to fill in the correct details
+        </Typography>
+        <Divider className="my-5 md:mb-12" />
+        <div className="flex items-start flex-col justify-start gap-2 mb-3 md:mb-8 lg:mb-12">
+          <label
+            htmlFor="contact-us"
+            className="futura font-medium text-neutral-800"
+          >
+            Contact Us
+          </label>
+          <CustomInput
+            sx={{ maxWidth: "628px" }}
+            multiline
+            rows={3}
+            fullWidth
+            id="contact-us"
+            variant="outlined"
+            size="medium"
+          />
+        </div>
+        <Btn className="py-3 px-6">Send</Btn>
+        <Divider className="my-3 sm:my-5 md:my-8 lg:my-12" />
+        <Typography
+          variant="h4"
+          className="text-primary-700 futura font-medium"
+        >
+          FAQs
+        </Typography>
+        <div className="-mx-5 sm:-mx-10">
+          <Questions />
+        </div>
+      </div>
     </>
   );
 };
