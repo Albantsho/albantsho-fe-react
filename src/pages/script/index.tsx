4c12b3a3dfe49afb5a860e5545ce155a4d9cfd44
TheMetaverseEngineer <info@themetaverse.engineer>
2022-10-29 15:21:41 +0330

    add format menu to script page

--- src/pages/script/index.tsx
+++ src/pages/script/index.tsx
@@ -4,6 +4,7 @@ import CommentList from "components/Script/CommentList/CommentList";
 import CommentModal from "components/Script/CommentList/CommentModal/CommentModal";
 import ExportFile from "components/Script/ExportFile/ExportFile";
 import ExportFileModal from "components/Script/ExportFile/ExportFileModal/ExportFileModal";
+import ChangeFormatMenu from "components/Script/index/ChangeFormatMenu";
 import ScenesList from "components/Script/ScenesList/ScenesList";
 import ScenesListModal from "components/Script/ScenesList/ScenesListModal/ScenesListModal";
 import ScriptDocument from "components/Script/ScriptDocument/ScriptDocument";
@@ -11,9 +12,33 @@ import ScriptDocumentModal from "components/Script/ScriptDocument/ScriptDocument
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { NextPageWithLayout } from "pages/_app";
+import { useState } from "react";
 
 const Script: NextPageWithLayout = () => {
   const { query } = useRouter();
+  const [contextMenu, setContextMenu] = useState<{
+    mouseX: number;
+    mouseY: number;
+  } | null>(null);
+
+  const handleContextMenu = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setContextMenu(
+      contextMenu === null
+        ? {
+            mouseX: event.clientX + 2,
+            mouseY: event.clientY - 6,
+          }
+        : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
+          // Other native context menus might behave different.
+          // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
+          null
+    );
+  };
+
+  const handleClose = () => {
+    setContextMenu(null);
+  };
 
   return (
     <>
@@ -33,7 +58,11 @@ const Script: NextPageWithLayout = () => {
       {/* <CommentModal /> */}
       {/* <ScriptDocumentModal /> */}
 
-      <div className=" w-full px-10 flex flex-col">
+      <div
+        onContextMenu={handleContextMenu}
+        style={{ cursor: "context-menu" }}
+        className="w-full px-10 flex flex-col"
+      >
         <div className="bg-white max-w-3xl py-14">
           <Typography className="text-center futura font-bold">
             LONG MAN OF LONG BEACH
@@ -54,6 +83,7 @@ const Script: NextPageWithLayout = () => {
             corrupti voluptas ullam fugiat dignissimos eligendi dolore?
           </Typography>
         </div>
+        <ChangeFormatMenu contextMenu={contextMenu} handleClose={handleClose} />
       </div>
     </>
   );
