cc7ee6837f372384bd0106af85b1f27c32c999c8
shahram <shahramkhaleh2001@gmail.com>
2023-03-08 06:33:46 +0330

    debugging file title page

--- src/components/Script/Title/validation/title.validation.ts
+++ src/components/Script/Title/validation/title.validation.ts
@@ -1,8 +1,8 @@
 import * as Yup from "yup";
 
 export const titleSchema = Yup.object({
-  title: Yup.string().min(4).max(40).label("Title"),
-  writer: Yup.string().min(4).max(40).label("Written by"),
-  names: Yup.string().min(4).max(40).label("Names"),
-  basedOn: Yup.string().min(4).max(40).label("Based on"),
+  title: Yup.string().max(40).label("Title"),
+  writer: Yup.string().max(40).label("Written by"),
+  names: Yup.string().max(40).label("Names"),
+  basedOn: Yup.string().max(40).label("Based on"),
 });
