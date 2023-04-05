f0cb86702d5eb79f1ec1df17b3e3bfabf1db56a4
shahram <shahramkhaleh2001@gmail.com>
2023-04-05 19:38:01 +0430

    fix: change  theme meta tag value to project primary color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* <meta name="theme-color" content={theme.palette.secondary.main} /> */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
