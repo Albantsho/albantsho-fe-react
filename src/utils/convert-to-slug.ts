432d7babefd21179b3dba0d40c0d6a5f72e35ebf
shahram <shahramkhaleh2001@gmail.com>
2023-05-11 19:05:51 +0430

    chore: add comment;

--- src/utils/convert-to-slug.ts
+++ src/utils/convert-to-slug.ts
@@ -1,6 +1,12 @@
-//* create slug from title for blog
+//* This function is used to convert a given string to a slug format, which can be used 
+//* to create more SEO-friendly URLs for blog posts.
+
 export const convertToSlug = (Text: string) => {
+  //* Converts all characters in the string to lowercase.
+  //* This is done to maintain consistency and prevent issues related to capital letters.
   return Text.toLowerCase()
+    //* Replaces all occurrences of spaces " " with hyphens "-" using a regular expression.
     .replace(/ /g, "-")
+    //* Removes any non-alphanumeric characters except for hyphen "-", again using a regular expression.
     .replace(/[^\w-]+/g, "");
 };
