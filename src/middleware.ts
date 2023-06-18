3b78b13bb94749df606aaa17b5ff709b6d9fa54f
shahram <shahramkhaleh2001@gmail.com>
2023-06-18 04:56:12 +0430

    feat; persist user routes by role type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,15 @@
+import jwt from "jsonwebtoken";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import routes from "utils/routes";
 
 export function middleware(request: NextRequest) {
   const refreshToken = request.cookies.get("refreshToken");
-  const accessToken = request.cookies.get("refreshToken");
+  const accessToken = request.cookies.get("accessToken");
+  const tokenPayload: any = jwt.decode(refreshToken?.value || "");
+
+  console.log(tokenPayload && tokenPayload);
+
 
   //* registration routes
   if (request.nextUrl.pathname.startsWith(routes.register.url)) {
@@ -38,20 +43,54 @@ export function middleware(request: NextRequest) {
     }
   }
 
+  //* writer routes
+  if (request.nextUrl.pathname.includes(routes.writerDashboard.url)) {
+    if (!refreshToken) {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
 
-
-
-
-
-
-
-
-
-  if (request.nextUrl.pathname.startsWith("/authentication")) {
-    if (refreshToken) {
-      return NextResponse.rewrite(new URL("/", request.url));
+    }
+    if (tokenPayload && tokenPayload.userType !== "writer") {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
     }
   }
+
+  //* producer routes
+  if (request.nextUrl.pathname.includes(routes.producerDashboard.url)) {
+    if (!refreshToken) {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+
+    }
+    if (tokenPayload && tokenPayload.userType !== "producer") {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+    }
+  }
+
+  //* reviewer routes
+  if (request.nextUrl.pathname.includes(routes.reviewerDashboard.url)) {
+    if (!refreshToken) {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+
+    }
+    if (tokenPayload && tokenPayload.userType !== "reviewer") {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+    }
+  }
+
+  //* admin routes
+  if (request.nextUrl.pathname.includes(routes.adminDashboard.url)) {
+    if (!refreshToken) {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+
+    }
+    if (tokenPayload && tokenPayload.userType !== "admin") {
+      return NextResponse.rewrite(new URL(routes.home.url, request.url));
+    }
+  }
+
+
+
+
+
 
   // return NextResponse.rewrite(new URL('/dashboard/user', request.url));
 
